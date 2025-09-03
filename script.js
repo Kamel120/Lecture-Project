cade5b1b8227433c8464642073529959346ed91d
KamelMohamedKamel <KamelMohamedKamel@eng.kfs.edu.eg>
2025-09-03 12:11:42 +0300

    Added mouseove,mouseout functionalities in JS in Toot branch

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".container").addEventListener("mouseover",function(){
+    event.target.style.backgroundColor = "blue";
+})
+
+document.querySelector(".container").addEventListener("mouseout",function(){
+    event.target.style.backgroundColor = "White";
+})
